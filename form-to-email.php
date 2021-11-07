20c211e3a34e40bfbd544d33735f07bea18735f1
Iva <samaya@mail.bg>
2021-11-07 17:12:24 +0100

    contact form tweaks

--- form-to-email.php
+++ form-to-email.php
@@ -33,7 +33,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: contact.html');
+header('Location: contact-confirm.html');
 
 
 // Function to validate against any email injection attempts
